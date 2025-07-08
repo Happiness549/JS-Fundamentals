4bf3cfcb82cff5da3323c0e25b28c17652f9753a
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-07-08 12:36:17 +0200

    Add 2-arguments.js file and the if statement inside

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const arguments = 0;
+if(arguments === 0){
+    console.log("No arguments");
+} else if("arguments === 1"){
+    console.log("Argument found")
+} else{
+    console.log("Arguments found");
+}
